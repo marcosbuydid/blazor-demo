e370e1ef83b081a8c522c047ab69c7580ce67989
marcos <marcosbuydid@gmail.com>
2024-10-07 13:21:55 -0300

    create simple login flow

---
+++ DA12024/Logic/Models/UserLoginDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Models
+{
+    public class UserLoginDTO
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+        public UserLoginDTO(String email, String password)
+        { 
+            Email = email;
+            Password = password;
+        }
+    }
+}
